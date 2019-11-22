aca2a0a223f87fafe54c3e3c0440e8505a1032b8
Serozar <a>
2019-11-22 02:15:17 +0000

    Started testing the profile module
    profile module junit tests

---
+++ Application/Stonks/test/controllers/ProfileControllerTest.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Bizarro
+ */
+public class ProfileControllerTest {
+    
+    public ProfileControllerTest() {
+    }
+
+    /**
+     * Test of stonksHasProfiles method, of class ProfileController.
+     */
+    @Test
+    public void testStonksHasProfiles() {
+    }
+
+    /**
+     * Test of profileIsLogedIn method, of class ProfileController.
+     */
+    @Test
+    public void testProfileIsLogedIn() {
+    }
+
+    /**
+     * Test of getNextId method, of class ProfileController.
+     */
+    @Test
+    public void testGetNextId() {
+    }
+
+    /**
+     * Test of getProfileById method, of class ProfileController.
+     */
+    @Test
+    public void testGetProfileById() {
+    }
+
+    /**
+     * Test of isFirstNameValid method, of class ProfileController.
+     */
+    @Test
+    public void testIsFirstNameValid() {
+    }
+
+    /**
+     * Test of isLastNameValid method, of class ProfileController.
+     */
+    @Test
+    public void testIsLastNameValid() {
+    }
+
+    /**
+     * Test of isSecurityQuestionValid method, of class ProfileController.
+     */
+    @Test
+    public void testIsSecurityQuestionValid() {
+    }
+
+    /**
+     * Test of isSecurityAnswerValid method, of class ProfileController.
+     */
+    @Test
+    public void testIsSecurityAnswerValid() {
+    }
+
+    /**
+     * Test of isPasswordValid method, of class ProfileController.
+     */
+    @Test
+    public void testIsPasswordValid() {
+    }
+
+    /**
+     * Test of isColorValid method, of class ProfileController.
+     */
+    @Test
+    public void testIsColorValid() {
+    }
+
+    /**
+     * Test of registerProfile method, of class ProfileController.
+     */
+    @Test
+    public void testRegisterProfile() {
+    }
+
+    /**
+     * Test of loginProfile method, of class ProfileController.
+     */
+    @Test
+    public void testLoginProfile() {
+    }
+
+    /**
+     * Test of logoutProfile method, of class ProfileController.
+     */
+    @Test
+    public void testLogoutProfile() {
+    }
+
+    /**
+     * Test of recoverPassword method, of class ProfileController.
+     */
+    @Test
+    public void testRecoverPassword() {
+    }
+
+    /**
+     * Test of editProfile method, of class ProfileController.
+     */
+    @Test
+    public void testEditProfile() {
+    }
+    
+}
